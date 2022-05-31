9957a9c945ebbaf839433de891e6ff6d5bf7ad50
lyx <a7583779@gmail.com>
2022-05-31 16:30:39 +0800

    第一題:背景繪製 v.1

---
+++ app/src/main/java/com/example/app/MySurfaceView.kt
@@ -0,0 +1,44 @@
+package com.example.app
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import androidx.core.graphics.plus
+
+class MySurfaceView(context: Context?, attrs: AttributeSet?)
+    : SurfaceView(context, attrs), SurfaceHolder.Callback{
+    var surfaceHolder: SurfaceHolder
+    var BG: Bitmap
+
+    init {
+        surfaceHolder = getHolder()
+        BG = BitmapFactory.decodeResource(getResources(), R.drawable.background)
+        surfaceHolder.addCallback(this)
+    }
+
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        var canvas: Canvas = surfaceHolder.lockCanvas()
+        drawSomething(canvas)
+        surfaceHolder.unlockCanvasAndPost(canvas)
+    }
+
+    fun drawSomething(canvas: Canvas) {
+        //canvas.drawBitmap(BG, 0f, 0f, null)
+        var SrcRect:Rect = Rect(0, 0, BG.width, BG.height) //使用者看到的範圍
+        var w:Int = width
+        var h:Int = height
+        var DestRect: Rect = Rect(0, 0, w, h)
+        canvas.drawBitmap(BG, SrcRect, DestRect, null)
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        //TODO("Not yet implemented")
+    }
+}
