f6e49fbe306654a18f53de323d76a6fcf72dfe88
lyx <a7583779@gmail.com>
2022-06-06 09:42:41 +0800

    第四題:飛行員飛行及上下拖曳 v.4

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -5,11 +5,14 @@ import android.graphics.Canvas
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.SurfaceView
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.core.view.ViewCompat.getX
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
@@ -22,7 +25,8 @@ import kotlin.properties.Delegates
 @GlideModule
 public final class MyAppGlideModule : AppGlideModule()
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    GestureDetector.OnGestureListener{
 
     var btn:Boolean = true
     var cnt:Int = 1
@@ -30,6 +34,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnSt:ImageView
     lateinit var sv:MySurfaceView
     lateinit var img:ImageView
+    lateinit var fly:ImageView
+    lateinit var gd:GestureDetector
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +45,8 @@ class MainActivity : AppCompatActivity() {
 
         btnSt = findViewById(R.id.btnSt)
         sv = findViewById(R.id.sv)
+        fly = findViewById(R.id.fly)
+        gd = GestureDetector(this,this)
 
         btnSt.setOnClickListener(object:View.OnClickListener{
             override fun onClick(p0: View?) {
@@ -53,8 +61,12 @@ class MainActivity : AppCompatActivity() {
                             sv.drawSomething(canvas)
                             sv.holder.unlockCanvasAndPost(canvas)
                             delay(20)
-                        }
 
+                            fly.setImageResource(R.drawable.fly2)
+                            delay(20)
+                            fly.setImageResource(R.drawable.fly1)
+                            delay(20)
+                        }
                     }
                 }
                 else{
@@ -72,6 +84,38 @@ class MainActivity : AppCompatActivity() {
             .circleCrop()
             .override(1600,1200)
             .into(img)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if(btn == true){
+            gd.onTouchEvent(event)
+        }
+        return true
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean { //短按，每次點擊時發生
+        return true
+    }
+
+    override fun onShowPress(e: MotionEvent?) { //使用者點擊後，停留較長一點時間，沒有滑動也還沒放開時發生
+
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {  //短按
+        return true
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {    //拖曳
+        fly.y = e2!!.y
+        return true
+    }
+
+    override fun onLongPress(e: MotionEvent?) { //長按
+
+    }
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean { //快滑
+        return true
     }
 }
 
